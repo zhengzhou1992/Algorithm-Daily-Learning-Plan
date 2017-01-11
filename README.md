3dc662b3d135e0e5f112d2fa1869f3afb155c03c
zhengzhou1992 <joezheng1992@gmail.com>
2017-01-12 00:44:53 +0800

    add script for auto modifing README according new added file name

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Algorithm-Daily-Learning-Plan
 It records the progress of my learning algorithm.
+
+## One day one file:
+* [implementing the checkerboard covering](https://github.com/zhengzhou1992/Algorithm-Daily-Learning-Plan/blob/master/implementing-the-checkerboard-covering.py)
---
+++ README_auto_link.py
@@ -0,0 +1,21 @@
+from os import listdir
+from os.path import abspath, dirname, join
+
+URL_BASE = 'https://github.com/zhengzhou1992/Algorithm-Daily-Learning-Plan/blob/master/'
+SECTION_SEPARATOR = '## One day one file:'
+BASE_DIR = dirname(abspath(__file__))
+
+with open(join(BASE_DIR, 'README.md'), 'r+') as readme:
+    append_pos = readme.read().find(SECTION_SEPARATOR)
+    readme.truncate(append_pos + len(SECTION_SEPARATOR) + 3)
+    readme.seek(0, 2)
+    readme.write('\n')
+    for filename in listdir(BASE_DIR):
+        if '-' not in filename or not filename.endswith('.py'):
+            continue
+        show_name = filename[:-3].replace('-', ' ')
+        new_line = '* [{show_name}]({url_base}{filename})\n'.format(
+            show_name=show_name,
+            url_base=URL_BASE,
+            filename=filename)
+        readme.write(new_line)
