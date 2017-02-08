b1bab1b5a1a4478c7f4e4f31d3432d94c56c05cf
zhengzhou1992 <joezheng1992@gmail.com>
2017-02-09 00:57:04 +0800

    add iddfs / bfs

--- dfs-iddfs-bfs.py
+++ dfs-iddfs-bfs.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def rec_dfs(G, s, S=None):
     if S is None:
         S = set()
@@ -14,10 +17,75 @@ def iter_dfs(G, s):
         u = Q.pop()
         if u in S:
             continue
-        print(G[u])
         Q.extend(G[u])
         S.add(u)
         yield u
+
+
+def graph_traverse(G, s, qtype=set):
+    S, Q = set(), qtype()
+    Q.add(s)
+    while Q:
+        u = Q.pop()
+        if u in S:
+            continue
+        for v in G[u]:
+            Q.add(v)
+        S.add(u)
+        yield u
+
+
+def timing_dfs(G, s, d, f, S=None, t=0):
+    if S is None:
+        S = set()
+    d[s] = t
+    t += 1
+    S.add(s)
+    for v in G[s]:
+        if v in S:
+            continue
+        t = timing_dfs(G, v, d, f, S, t)
+    f[s] = t
+    t += 1
+    return t
+
+
+def iddfs(G, s):
+    yielded = set()
+
+    def recurse(G, s, d, S=None):
+        if s not in yielded:
+            yield s
+            yielded.add(s)
+        if d == 0:
+            return
+        if S is None:
+            S = set()
+        S.add(s)
+        for x in G[s]:
+            if x in S:
+                continue
+            for u in recurse(G, x, d - 1, S):
+                yield u
+
+    n = len(G)
+    for d in range(n):
+        if len(yielded) == n:
+            break
+        for x in recurse(G, s, d):
+            yield x
+
+
+def bfs(G, s):
+    P, Q = {s: None}, deque([s])
+    while Q:
+        u = Q.popleft()
+        for v in G[u]:
+            if v in P:
+                continue
+            P[v] = u
+            Q.append(v)
+    return P
 
 
 if __name__ == '__main__':
@@ -29,3 +97,19 @@ if __name__ == '__main__':
     }
 
     print(list(iter_dfs(G, 'a')))
+    print(list(graph_traverse(G, 'a')))
+
+    start_time = dict()
+    finish_time = dict()
+    timing_dfs(G, 'a', start_time, finish_time)
+    print(start_time, finish_time)
+
+    print(list(iddfs(G, 'a')))
+
+    P = bfs(G, 'a')
+    a = 'c'
+    path = [a]
+    while P[a] is not None:
+        path.append(P[a])
+        a = P[a]
+    print(path)
